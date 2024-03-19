46e6276cad5c1d39b6817826af29514efde34c9c
dlansrhks75 <dlansrhks75@naver.com>
2024-03-19 16:43:56 +0900

    BoardController 수정[이문관]

--- hello_c/src/main/java/com/example/demo/controller/BoardController.java
+++ hello_c/src/main/java/com/example/demo/controller/BoardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 	@GetMapping("/listBoard")
 	public String BoardController() {
-		System.out.println("1");
+		System.out.println("2");
 		return "게시물 목록!!!!!!!!!!!!!!!!!!!";
 	}
 }
