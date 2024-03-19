b0aa2c663c7f3a3aaa202e790e8f024c6462475e
xo-hyun <xo_hyunn@naver.com>
2024-03-19 15:36:50 +0900

    msg!!!

---
+++ hello_c/src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+	@GetMapping("/listBoard")
+	public String BoardController() {
+		return "게시물 목록!!!!!!!!!!!!!!!!!!!";
+	}
+}
